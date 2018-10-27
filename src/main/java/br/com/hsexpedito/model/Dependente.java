e2a77d1e7ae3499d3e485c1f53ace94768657f99
Gabriel Peixoto <gpeixoto@gmail.com>
2018-10-27 20:12:13 -0300

    indentando... control shift F

--- src/main/java/br/com/hsexpedito/model/Dependente.java
+++ src/main/java/br/com/hsexpedito/model/Dependente.java
@@ -1,28 +1,32 @@
 package br.com.hsexpedito.model;
 
 public class Dependente {
-	
+
 	private int id;
 	private String nome;
 	private String parentesco;
 	private Funcionario funcionario;
-	
-	
+
 	public int getId() {
 		return id;
 	}
+
 	public void setId(int id) {
 		this.id = id;
 	}
+
 	public String getNome() {
 		return nome;
 	}
+
 	public void setNome(String nome) {
 		this.nome = nome;
 	}
+
 	public String getParentesco() {
 		return parentesco;
 	}
+
 	public void setParentesco(String parentesco) {
 		this.parentesco = parentesco;
 	}
--- src/main/java/br/com/hsexpedito/model/Funcionario.java
+++ src/main/java/br/com/hsexpedito/model/Funcionario.java
@@ -3,43 +3,51 @@ package br.com.hsexpedito.model;
 import java.util.List;
 
 public class Funcionario {
-	
+
 	private int id;
 	private String nome;
 	private String email;
 	private String telefone;
-    private List<Dependente> dependentes;
-    
-    
-    public int getId() {
+	private List<Dependente> dependentes;
+
+	public int getId() {
 		return id;
 	}
+
 	public void setId(int id) {
 		this.id = id;
 	}
+
 	public String getNome() {
 		return nome;
 	}
+
 	public void setNome(String nome) {
 		this.nome = nome;
 	}
+
 	public String getEmail() {
 		return email;
 	}
+
 	public void setEmail(String email) {
 		this.email = email;
 	}
+
 	public String getTelefone() {
 		return telefone;
 	}
+
 	public void setTelefone(String telefone) {
 		this.telefone = telefone;
 	}
+
 	public List<Dependente> getDependentes() {
 		return dependentes;
 	}
+
 	public void setDependentes(List<Dependente> dependentes) {
 		this.dependentes = dependentes;
 	}
-	
+
 }
