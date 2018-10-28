8f26500c5c97cb88eeaf8e47231691b2d5555228
Gabriel Peixoto <gpeixoto@gmail.com>
2018-10-28 13:14:27 -0300

    adicionando update de funcionario

--- src/main/java/br/com/hsexpedito/dao/FuncionarioDAO.java
+++ src/main/java/br/com/hsexpedito/dao/FuncionarioDAO.java
@@ -11,11 +11,11 @@ public class FuncionarioDAO {
 
 	public void cadastar(Funcionario f) {
 
-		String sql = "INSERT INTO tb_funcionario (nome, email, tel)" + "VALUES(?,?,?)";
+		String insertSql = "INSERT INTO tb_funcionario (nome, email, tel)" + "VALUES(?,?,?)";
 
 		try (Connection conexao = ConnectionMySql.openDatabase()) {
 
-			PreparedStatement ps = conexao.prepareStatement(sql);
+			PreparedStatement ps = conexao.prepareStatement(insertSql);
 
 			ps.setString(1, f.getNome());
 			ps.setString(2, f.getEmail());
@@ -26,6 +26,25 @@ public class FuncionarioDAO {
 		} catch (SQLException e) {
 			e.printStackTrace();
 			System.out.println("Erro na insercao no banco de dados!");
+		}
+	}
+
+	public void atualizar(int id, String nome, String email, String telefone) {
+
+		String updateSql = "UPDATE tb_funcionario SET idfuncionario = ?, nome = ?, email = ?, tel = ? WHERE idfuncionario = ?";
+
+		try (Connection conexao = ConnectionMySql.openDatabase()) {
+
+			PreparedStatement ps = conexao.prepareStatement(updateSql);
+
+			ps.setInt(1, id);
+			ps.setString(2, nome);
+			ps.setString(3, email);
+			ps.setString(4, telefone);
+
+		} catch (SQLException e) {
+			e.printStackTrace();
+			System.out.println("Erro ao atualizar funcionario no banco de dados!");
 		}
 	}
 	// BUSCAR POR ID - 1 FUNC + SEUS DEPENDENTES
---
+++ src/main/java/br/com/hsexpedito/dao/test/funcionario/TestFuncionarioAtualizar.java
@@ -0,0 +1,13 @@
+package br.com.hsexpedito.dao.test.funcionario;
+
+public class TestFuncionarioAtualizar {
+
+	public static void main(String[] args) {
+		
+		
+		
+
+
+	}
+
+}
